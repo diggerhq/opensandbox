21c8aa79d544d68658fe46ba70694afab2d4d269
Brian Reardon <breardon2011@gmail.com>
2026-05-07 13:14:05 -0700

    fix healthz unreachable

--- ci/pr/up.sh
+++ ci/pr/up.sh
@@ -83,6 +83,7 @@ if ! az vm show -g "$RG" -n "$SERVER_VM" -o none 2>/dev/null; then
     --admin-username azureuser \
     --ssh-key-values "$SSH_KEY_PUB" \
     --os-disk-size-gb 32 \
+    --nsg "" \
     --tags purpose=ci pr="$PR_NUM" \
     -o none
 fi
